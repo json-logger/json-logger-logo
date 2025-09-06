b50b7b3d4697270e4ea13f7d86645f7a3b718f55
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2025-09-06 12:26:38 +0530

    fix: JPG output image (required by GitHub)

--- logo.py
+++ logo.py
@@ -6,5 +6,5 @@ import numpy as np
 plt.rc("font", family="Signika")
 fig = plt.figure(figsize=(4, 4))
 fig.text(0.5, 0.5, "{…}\no11y", ha="center", va="center", size=100)
-fig.savefig("logo.svg")
+fig.savefig("logo.jpg")
 plt.show()
