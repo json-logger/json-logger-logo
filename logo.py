fix: add gradient to logo and change text colour

logo.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.rc("font", family="Signika")
-fig = plt.figure(figsize=(4, 4))
-fig.text(0.5, 0.5, "{…}\no11y", ha="center", va="center", size=100)
-fig.savefig("logo.jpg")
+
+x = np.linspace(0, 1, 1023)
+y = x[::-1]
+x_grid, y_grid = np.meshgrid(x, y)
+z_grid = np.sqrt(x_grid**2 + y_grid**2)
+
+fig, ax = plt.subplots()
+ax.axis("off")
+ax.imshow(z_grid, cmap="Greys", vmin=0, vmax=2)
+ax.text(x_grid.shape[0] / 2, x_grid.shape[1] / 2, "{…}\no11y", color="#7F7F7F", ha="center", va="center", size=120)
+fig.savefig("logo.jpg", pad_inches=0)
 plt.show()